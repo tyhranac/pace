5a5815328178e74ca5a00e69032c694051f4c8a8
ty.hranac <ty.hranac@gmail.com>
2022-12-18 19:08:00 -0800

    Set up query selectors and functions for calculations

--- app.js
+++ app.js
@@ -12,4 +12,31 @@ hamburgerMenu.addEventListener('click', () => {
     }
 });
 
-// form calcs
+// form
+const calculatePace = (time, distance) => {
+    return time/distance;
+}
+
+const calculateTime = (pace, distance) => {
+    return pace * distance;
+}
+
+const calculateDistance = (pace, time) => {
+    return time/pace;
+}
+
+const form = document.querySelector('form');
+const distance = document.querySelector('#dist');
+const distanceUnits = document.querySelector('#units-dist');
+const timeHours = document.querySelector('#time-hours');
+const timeMinutes = document.querySelector('#time-minutes');
+const timeSeconds = document.querySelector('#time-seconds');
+const paceHours = document.querySelector('#pace-hours');
+const paceMinutes = document.querySelector('#pace-minutes');
+const paceSeconds = document.querySelector('#pace-seconds');
+const paceUnits = document.querySelector('#units-pace');
+const submit = document.querySelector('#form-submit');
+
+form.addEventListener('submit', (event) => {
+
+})
